342b0923458d673f0b311fc614cd30464b672adb
Mox <alibudbuddev@gmail.com>
2021-02-17 17:46:43 +0400

    can create user

--- assignment-2/api/controllers/user-controller.js
+++ assignment-2/api/controllers/user-controller.js
@@ -4,20 +4,40 @@
  *
  */
 
-var requestHandler = require('./../util/request-handler');
+const requestHandler = require('./../util/request-handler');
+const validator = require('./../../lib/validator');
+const statusCode = require('./../../lib/status-code');
+const UserModel = require('./../models/user-model');
 
 // User controller container
-var _container  = {};
+const _container  = {};
 
 // Users - post
-// Required data: firstName, lastName, phone, password, tosAgreement
-// Optional data: none
+// Required data: name, email, address, password
 _container.post = function(data, callback){
-  console.log(data);
-  callback(200, data.payload);
+  const payload = data.payload;
+  if(
+    validator.isEmail(payload.email) &&
+    validator.isNotEmpty(payload.name) &&
+    validator.isNotEmpty(payload.address),
+    validator.isNotEmpty(payload.password)
+  ) {
+    const userModel = new UserModel(payload.email);
+    userModel.notExist(function(notExist) {
+      if(notExist) {
+        userModel.save(payload, function(statusCode, payload = {}) {
+          callback(statusCode, payload);
+        });
+      } else {
+        callback(statusCode.SERVER_ERROR, {'error' : 'User email already exists'});
+      }
+    });
+  } else {
+    callback(statusCode.NOT_FOUND, {'error' : 'Missing required fields'});
+  }
 }
 
-var userController = function(data, callback) {
+const userController = function(data, callback) {
   requestHandler.handler(data, _container, callback);
 }
 

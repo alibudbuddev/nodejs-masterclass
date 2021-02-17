f3e0e4b5f4048ec4c63e0515c023f0266661919f
Mox <alibudbuddev@gmail.com>
2021-02-17 18:17:24 +0400

    update data orm

--- assignment-2/lib/data-orm.js
+++ assignment-2/lib/data-orm.js
@@ -10,6 +10,10 @@ class DataORM {
     this.jsonFile = this.baseDir+this.model+'/'+this.primaryKey+'.json';
   }
 
+  /*
+   * Check if user data exists.
+   * @param {function} callback
+   */
   exists(callback) {
     fs.readFile(this.jsonFile, 'utf8', function(err) {
       if(err) {
@@ -20,6 +24,11 @@ class DataORM {
     });
   }
 
+  /*
+   * Create a new user data.
+   * @param {object} data - User object.
+   * @param {function} callback
+   */
   create(data, callback) {
     // Open the file for writing
     fs.open(this.jsonFile, 'wx', function(err, fileDescriptor) {
@@ -43,6 +52,56 @@ class DataORM {
         });
       } else {
         callback('Could not create new file, it may already exist');
+      }
+    });
+  
+  };
+
+  /*
+   * Delete user data.
+   * @param {function} callback
+   */
+  delete(callback) {
+    fs.unlink(this.jsonFile, function(err) {
+      callback(err);
+    });
+  };
+
+  /*
+   * Update user data.
+   * @param {object} data - User updates object.
+   * @param {function} callback
+   */
+  update(data, callback) {
+    // Open the file for writing
+    fs.open(this.jsonFile, 'r+', function(err, fileDescriptor) {
+      if(!err && fileDescriptor){
+        // Convert data to string
+        var stringData = JSON.stringify(data);
+  
+        // Truncate the file
+        fs.truncate(fileDescriptor,function(err) {
+          if(!err) {
+            // Write to file and close it
+            fs.writeFile(fileDescriptor, stringData, function(err) {
+              if(!err){
+                fs.close(fileDescriptor, function(err) {
+                  if(!err) {
+                    callback(false);
+                  } else {
+                    callback('Error closing existing file');
+                  }
+                });
+              } else {
+                callback('Error writing to existing file');
+              }
+            });
+          } else {
+            callback('Error truncating file');
+          }
+        });
+      } else {
+        callback('Could not open file for updating, it may not exist yet');
       }
     });
   

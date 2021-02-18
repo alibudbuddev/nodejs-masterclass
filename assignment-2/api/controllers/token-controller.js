e79bb4bbe018550967db44a204b81a9b36e802d0
Mox <alibudbuddev@gmail.com>
2021-02-18 15:00:58 +0400

    clean up token controller

--- assignment-2/api/controllers/token-controller.js
+++ assignment-2/api/controllers/token-controller.js
@@ -15,7 +15,6 @@ const { stat } = require('fs/promises');
 // Token controller container
 const _container = {};
 
-// Tokens - post
 // Required data: email, password
 _container.post = (data, callback) => {
   const payload = data.payload;
@@ -67,34 +66,7 @@ _container.get = function(data, callback) {
 
 // NOT NEEDED YET
 _container.put = function(data, callback) {
-  // var id = typeof(data.payload.id) == 'string' && data.payload.id.trim().length == 20 ? data.payload.id.trim() : false;
-  // var extend = typeof(data.payload.extend) == 'boolean' && data.payload.extend == true ? true : false;
-  // if(id && extend){
-  //   // Lookup the existing token
-  //   _data.read('tokens',id,function(err,tokenData){
-  //     if(!err && tokenData){
-  //       // Check to make sure the token isn't already expired
-  //       if(tokenData.expires > Date.now()){
-  //         // Set the expiration an hour from now
-  //         tokenData.expires = Date.now() + 1000 * 60 * 60;
-  //         // Store the new updates
-  //         _data.update('tokens',id,tokenData,function(err){
-  //           if(!err){
-  //             callback(200);
-  //           } else {
-  //             callback(500,{'Error' : 'Could not update the token\'s expiration.'});
-  //           }
-  //         });
-  //       } else {
-  //         callback(400,{"Error" : "The token has already expired, and cannot be extended."});
-  //       }
-  //     } else {
-  //       callback(400,{'Error' : 'Specified user does not exist.'});
-  //     }
-  //   });
-  // } else {
-  //   callback(400,{"Error": "Missing required field(s) or field(s) are invalid."});
-  // }
+  callback(statusCode.SUCCESS);
 }
 
 _container.delete = function(data, callback) {
@@ -113,22 +85,6 @@ _container.delete = function(data, callback) {
   } else {
     callback(statusCode.NOT_FOUND, helpers.errObject('Token invalid'))
   }
-}
-
-_container.verifyToken = function(id, phone, callback) {
-  // // Lookup the token
-  // _data.read('tokens',id,function(err,tokenData){
-  //   if(!err && tokenData){
-  //     // Check that the token is for the given user and has not expired
-  //     if(tokenData.phone == phone && tokenData.expires > Date.now()){
-  //       callback(true);
-  //     } else {
-  //       callback(false);
-  //     }
-  //   } else {
-  //     callback(false);
-  //   }
-  // });
 }
 
 var tokenController = function(data, callback) {

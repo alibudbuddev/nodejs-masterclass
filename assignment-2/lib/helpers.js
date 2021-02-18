c92a53e249a156eba3d0b120da522e373a571d03
Mox <alibudbuddev@gmail.com>
2021-02-18 13:29:53 +0400

    use arrow function in helpers

--- assignment-2/lib/helpers.js
+++ assignment-2/lib/helpers.js
@@ -11,7 +11,7 @@ var crypto = require('crypto');
 var helpers = {};
 
 // Parse a JSON string to an object in all cases, without throwing
-helpers.parseJsonToObject = function(str) {
+helpers.parseJsonToObject = (str) => {
   try{
     var obj = JSON.parse(str);
     return obj;
@@ -21,10 +21,34 @@ helpers.parseJsonToObject = function(str) {
 };
 
 // Create a SHA256 hash
-helpers.hash = function(str) {
+helpers.hash = (str) => {
   if(typeof(str) == 'string' && str.length > 0){
     var hash = crypto.createHmac('sha256', config.hashingSecret).update(str).digest('hex');
     return hash;
+  } else {
+    return false;
+  }
+};
+
+helpers.expiration = Date.now() + 1000 * 60 * 60;
+
+// Create a string of random alphanumeric characters, of a given length
+helpers.createRandomString = (strLength) => {
+  strLength = typeof(strLength) == 'number' && strLength > 0 ? strLength : false;
+  if(strLength){
+    // Define all the possible characters that could go into a string
+    var possibleCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    // Start the final string
+    var str = '';
+    for(i = 1; i <= strLength; i++) {
+        // Get a random charactert from the possibleCharacters string
+        var randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+        // Append this character to the string
+        str+=randomCharacter;
+    }
+    // Return the final string
+    return str;
   } else {
     return false;
   }

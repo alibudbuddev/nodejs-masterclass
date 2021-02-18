2810df6b68b811068897d2aeab98a33525428445
Mox <alibudbuddev@gmail.com>
2021-02-18 17:42:17 +0400

    Can return a product to authenticated user

--- assignment-2/api/controllers/product-controller.js
+++ assignment-2/api/controllers/product-controller.js
@@ -4,20 +4,32 @@
  *
  */
 
-var requestHandler = require('./../util/request-handler');
-
+ requestHandler = require('./../util/request-handler');
+ const statusCode = require('./../../lib/status-code');
+ 
 // Product controller container
-var _container = {};
+const _container = {};
 
-// Users - post
-// Required data: firstName, lastName, phone, password, tosAgreement
-// Optional data: none
-_container.post = function(data, callback){
-  console.log(data);
-  callback(200, data.payload);
+const products = [
+  { 'name': 'Cheese', 'price' : 2.50, 'location': 'Refrigerated foods'},
+  { 'name': 'Crisps', 'price' : 3, 'location': 'the Snack isle'},
+  { 'name': 'Pizza', 'price' : 4, 'location': 'Refrigerated foods'},
+  { 'name': 'Chocolate', 'price' : 1.50, 'location': 'the Snack isle'},
+  { 'name': 'Self-raising flour', 'price' : 1.50, 'location': 'Home baking'},
+  { 'name': 'Ground almonds', 'price' : 3, 'location': 'Home baking'}
+];
+
+_container.get = (data, callback) => {
+  // Check if token is valid
+  if(!data.tokenState.valid) {
+    callback(statusCode.UNAUTHORIZED);
+    return;
+  }
+
+  callback(statusCode.success, products);
 }
 
-var productController = function(data, callback) {
+const productController = (data, callback) => {
   requestHandler.handler(data, _container, callback);
 }
 

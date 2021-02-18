117dad121ef5390f5be10dfcc569aacd924ac227
Mox <alibudbuddev@gmail.com>
2021-02-18 14:58:22 +0400

    Refactor server to verify token

--- assignment-2/lib/server.js
+++ assignment-2/lib/server.js
@@ -1,44 +1,69 @@
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var routes = require('./../api/routes');
-var helpers = require('./helpers');
+const url = require('url');
+var fs = require('fs');
+var path = require('path');
+const StringDecoder = require('string_decoder').StringDecoder;
+const routes = require('./../api/routes');
+const helpers = require('./helpers');
 
-var server = function(req, res) {
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
+const server = function(req, res) {
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
 
   req.on('data', function(data) {
     buffer += decoder.write(data);
   });
   req.on('end', function() {
-    var requestMetadata = getRequestMetaData(req, buffer);
-    buffer += decoder.end();
+    verifyToken(req.headers, (state) => {
+      const requestMetadata = getRequestMetaData(req, buffer);
+      buffer += decoder.end();
 
-    // Generate handler based on URL Path
-    var handler = typeof(routes[requestMetadata.path]) !== 'undefined' ? routes[requestMetadata.path] : routes['not-found'];
+      // Generate handler based on URL Path
+      const handler = typeof(routes[requestMetadata.path]) !== 'undefined' ? routes[requestMetadata.path] : routes['not-found'];
 
-    // Route the request
-    handler(requestMetadata, function(statusCode, payload) {
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-      payload = typeof(payload) == 'object'? payload : {};
-      responseHandler(statusCode, payload, res);
+      // Pass token state
+      requestMetadata['tokenState'] = state;
+
+      // Route the request
+      handler(requestMetadata, function(statusCode, payload) {
+        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+        payload = typeof(payload) == 'object'? payload : {};
+        responseHandler(statusCode, payload, res);
+      });
     });
   });
 };
 
-var responseHandler = function(statusCode, payload, res) {
+const responseHandler = function(statusCode, payload, res) {
   res.setHeader('Content-Type', 'application/json');
   res.writeHead(statusCode);
   res.end(JSON.stringify(payload));
 }
 
-var getRequestMetaData = function(req, buffer) {
-  var parsedUrl = url.parse(req.url, true);
-  var path = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
-  var queryStringObject = parsedUrl.query;
-  var method = req.method.toLowerCase();
-  var headers = req.headers;
-  var payload = helpers.parseJsonToObject(buffer)
+const verifyToken = (headers, callback) => {
+  fs.readFile(`${path.join(__dirname,'/../.data/')}tokens/${headers.token}.json`, 'utf8',
+    (err, data) => {
+      var tokenData = helpers.parseJsonToObject(data);
+      if(!err && tokenData){
+        // Check the token if not expired
+        if(tokenData.expires > Date.now()){
+          callback({'valid': true});
+        } else {
+          callback({'valid': false});
+        }
+      } else {
+        callback({'valid': false});
+      }
+    }
+  );
+}
+
+const getRequestMetaData = function(req, buffer) {
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
+  const queryStringObject = parsedUrl.query;
+  const method = req.method.toLowerCase();
+  const headers = req.headers;
+  const payload = helpers.parseJsonToObject(buffer)
   return {path, queryStringObject, method, headers, payload};
 }
 

5eb09bca6e9a541543bff3bb4f541b298dc409b0
Mox <alibudbuddev@gmail.com>
2021-02-18 16:15:20 +0400

    refactor auth controller

--- assignment-2/api/controllers/auth-controller.js
+++ assignment-2/api/controllers/auth-controller.js
@@ -3,6 +3,7 @@
  * Auth Controller
  *
  */
+const requestHandler = require('./../util/request-handler');
 const statusCode = require('./../../lib/status-code');
 const helpers = require('./../../lib/helpers');
 const UserModel = require('./../models/user-model');
@@ -54,19 +55,7 @@ _container.delete = function(data, callback) {
 }
 
 const authController = (data, callback) => {
-  switch (data.method) {
-    case 'post':
-      _container.post(data, callback);
-      break;
-
-    case 'delete':
-      _container.delete(data, callback);
-      break;
-  
-    default:
-      callback(statusCode.NOT_FOUND);
-      break;
-  }
+  requestHandler.handler(data, _container, callback);
 }
 
 // Export the controller

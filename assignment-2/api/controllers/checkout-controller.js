ccde553b2d92833e5440c4d552efc5f8ace048b0
Mox <alibudbuddev@gmail.com>
2021-02-22 16:43:30 +0400

    send receipt mail on checkout

--- assignment-2/api/controllers/checkout-controller.js
+++ assignment-2/api/controllers/checkout-controller.js
@@ -9,7 +9,9 @@ const OrderModel = require('./../models/order-model');
 const statusCode = require('./../../lib/status-code');
 const helpers = require('../../lib/helpers');
 const Stripe = require('./../../lib/stripe');
+const Mailgun = require('./../../lib/mailgun');
 const stripe = new Stripe();
+const mailgun = new Mailgun();
 
 var _container = {};
 
@@ -44,7 +46,23 @@ _container.post = function(data, callback){
             orderModel.update(orderDetailsPayload, (err, payload) => {
               if(err) {
                 console.error(err, payload);
+              } else {
+                // Email the receipt to the user only if order update is successful
+                console.log(`Thanks you! Your order with the amount of ${orderModel.data.total} has been placed.`);
+                const emailData = {
+                  from: 'test@nodejsmasterclass.com',
+                  to: tokenState.token.email,
+                  subject: `Purchase receipt #${stripePayload.payload.id}`,
+                  text: `Thanks you! Your order with the amount of ${orderModel.data.total} has been placed.`
+                };
+                mailgun.send(emailData, (err) => {
+                  if(err) {
+                    console.log(`Receipt is not sent to ${tokenState.token.email}`);
+                  }
+                });
               }
+
+              // Callback to client whenever the order us get updated.
               callback(stripePayload.statusCode);
             });
           } else {
